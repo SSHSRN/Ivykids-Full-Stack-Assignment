bd294d85a4dca00a53279fa22c8e26af4564285e
Srihari <sshsrn@gmail.com>
2023-09-14 20:25:35 +0530

    fix: format
    Signed-off-by: Srihari <sshsrn@gmail.com>

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, World!</h1>');
+    res.send('<h1>Hello, World!</h1>');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
